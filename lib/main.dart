561679ff4500147b5480ffc5d518392682983d3f
Surena Karimpour Ghannadi <suren.karimp@gmail.com>
2024-03-04 12:47:03 +0330

    Using GPLv3

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
-/*
-This is the main entry point of the program, serving as an initializer. 
-  Copyright (C) 2024  Surena Karimpour Ghannadi
+// Copyright (C) 2024 Surena Karimpour Ghannadi
+// 
+// This file is part of Prject Azhi.
+// 
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <http://www.gnu.org/licenses/>.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    */
 import 'package:flutter/material.dart';
-
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:matrix/matrix.dart';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
