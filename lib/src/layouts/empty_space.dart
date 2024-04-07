14476e015f5f5ebab3bfd1ac530f7c501b284b4d
Surena Karimpour Ghannadi <suren.karimp@gmail.com>
2024-04-07 22:43:27 +0330

    Widget for places where we don't show anything

---
+++ lib/src/layouts/empty_space.dart
@@ -0,0 +1,34 @@
+// Copyright (C) 2024 Surena Karimpour Ghannadi
+//
+// This file is part of Prject Azhi.
+//
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:flutter/material.dart';
+import 'package:azhi_main/src/widgets/themedLogoAndText.dart';
+
+class Empty extends StatelessWidget {
+  const Empty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+      ),
+      body: const LogoWithTextThemed(),
+    );
+  }
+}
