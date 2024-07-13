71032016b0edc52dff40565b4429ac774fa74d9c
Surena Karimpour Ghannadi <suren.karimp@gmail.com>
2024-07-13 16:06:38 +0430

    Cleanup and chatlist UI

---
+++ lib/src/widgets/spaces_pane.dart
@@ -0,0 +1,136 @@
+// Copyright (C) 2024 Surena Karimpour Ghannadi
+//
+// This file is part of Prject Azhi.
+//
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:azhi_main/src/widgets/large_bar_button.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' as material;
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
+import 'package:logger/logger.dart';
+import 'package:matrix/matrix.dart';
+import 'package:provider/provider.dart';
+
+class SpacesPane extends StatelessWidget {
+  const SpacesPane({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    void join(Room room) async {
+      try {
+        if (room.membership != Membership.join) {
+          await room.join();
+        }
+        context.push('/rooms/${room.id}');
+      } catch (e) {
+        Provider.of<Logger>(context).f(
+          "Failed to join",
+          error: e,
+          stackTrace: StackTrace.current,
+          time: DateTime.now(),
+        );
+        // FIXME: Better error and localization
+        await displayInfoBar(
+          context,
+          builder: (context, close) {
+            return InfoBar(
+              title: Text(AppLocalizations.of(context)!.error),
+              content: Text(e.toString()),
+              action: IconButton(
+                icon: const Icon(FluentIcons.clear),
+                onPressed: close,
+              ),
+              severity: InfoBarSeverity.error,
+            );
+          },
+        );
+      }
+    }
+
+    Client client = Provider.of<Client>(context);
+    String? ownDisplayName;
+    Future<void> getOwnDisplayName() async {
+      ownDisplayName = await client.getDisplayName(client.userID!);
+    }
+
+    return Column(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              backgroundBlendMode: BlendMode.darken, color: Colors.grey),
+          child: Column(
+            children: [
+              LargeBarButton(
+                onTap: () {},
+                title: ownDisplayName ?? "Your Profile",
+                icon: const Icon(material.Icons.person_outline_sharp),
+              ),
+              LargeBarButton(
+                onTap: () {},
+                title: "Direct Messages",
+                icon: const Icon(material.Icons.messenger_outline_sharp),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Expanded(
+          child: StreamBuilder(
+            stream: client.onSync.stream,
+            builder: (context, _) => ListView.builder(
+              itemCount: client.rooms.length,
+              itemBuilder: (context, index) => ListTile.selectable(
+                leading: CircleAvatar(
+                  foregroundImage: client.rooms[index].avatar == null
+                      ? null
+                      : NetworkImage(
+                          client.rooms[index].avatar.toString(),
+                        ),
+                ),
+                title: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        client.rooms[index].getLocalizedDisplayname(),
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: Text(
+                  client.rooms[index].lastEvent?.body ?? 'No messages',
+                  maxLines: 1,
+                ),
+                trailing: (client.rooms[index].notificationCount > 0)
+                    ? badges.Badge(
+                        child: Text(
+                          client.rooms[index].notificationCount.toString(),
+                        ),
+                      )
+                    : null,
+                onPressed: () => join(client.rooms[index]),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
