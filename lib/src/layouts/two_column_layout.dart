9ee2978fa65fc9c449157d72c9b6eafe2697ff0c
Surena Karimpour Ghannadi <suren.karimp@gmail.com>
2024-08-17 01:43:36 +0330

    Minor update

--- lib/src/layouts/two_column_layout.dart
+++ lib/src/layouts/two_column_layout.dart
@@ -1,6 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart' as mt;
-import 'package:flutter/widgets.dart';
 
 class TwoColumnLayout extends StatelessWidget {
   final Widget mainView;
@@ -17,10 +16,10 @@ class TwoColumnLayout extends StatelessWidget {
       body: Row(
         children: [
           Container(
+            decoration: const BoxDecoration(),
             clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(),
             width: 420,
-            child: Mica(child: mainView),
+            child: mainView,
           ),
           const Divider(
             direction: Axis.vertical,
