cf85a202526eae1e2317c908c04087f6896bc1c1
Surena Karimpour Ghannadi <suren.karimp@gmail.com>
2024-08-17 02:13:24 +0330

    Temporarily change to show OS window frame

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ void main() async {
     await WindowManager.instance.ensureInitialized();
     windowManager.waitUntilReadyToShow().then((_) async {
       await windowManager.setTitleBarStyle(
-        TitleBarStyle.hidden,
+        TitleBarStyle.normal,
         windowButtonVisibility: false,
       );
       await windowManager.setMinimumSize(const Size(500, 600));
