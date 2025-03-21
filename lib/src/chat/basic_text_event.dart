SurenaSKQ/moonrelay

commit 441604859be516e91cdd045e260c0f8367e6259f

feat(App Skeleton): :lipstick: Application frame rework
Completely rework the mother frame to use ScaffoldPage if on desktop. This lays the ground for a potential of porting to other display factors. FluentUI's own NavigationView just doesn't work well with this app's paradigm.

lib/src/layouts/desktop_app_frame.dart
@@ -1,0 +1,127 @@
+// Copyright (C) 2024 Surena Karimpour Ghannadi
+//
+// This file is part of Prject Azhi.
+//
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:blurrycontainer/blurrycontainer.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:azhi_main/src/widgets/window_buttons.dart';
+
+class AzhiAppFrame extends StatefulWidget {
+  const AzhiAppFrame({
+    super.key,
+    required this.child,
+    required this.shellContext,
+  });
+
+  final Widget child;
+  final BuildContext? shellContext;
+  @override
+  State<AzhiAppFrame> createState() => _AzhiAppFrameState();
+}
+
+class _AzhiAppFrameState extends State<AzhiAppFrame> with WindowListener {
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //STUB - For future!
+    // final TextEditingController searchController = TextEditingController();
+    // final settingsController = Provider.of<SettingsController>(context);
+
+    return Stack(
+      children: [
+        DragToResizeArea(
+          child: Container(
+            decoration: BoxDecoration(
+              color: FluentTheme.of(context).accentColor,
+            ),
+          ),
+        ),
+        ScaffoldPage.withPadding(
+          padding: const EdgeInsets.all(1.5),
+          header: DragToMoveArea(
+            child: BlurryContainer(
+              color: FluentTheme.of(context).accentColor,
+              borderRadius: const BorderRadius.all(Radius.zero),
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional.centerStart,
+                    child: Text(
+                      AppLocalizations.of(context)!.appTitle,
+                      style: const TextStyle(
+                        fontFamily: 'JetBrainsMono',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  const WindowButtons(),
+                ],
+              ),
+            ),
+          ),
+          content: widget.child,
+        ),
+      ],
+    );
+  }
+
+  @override
+  void onWindowClose() async {
+    bool isPreventClose = await windowManager.isPreventClose();
+    if (isPreventClose && mounted) {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return ContentDialog(
+            title: Text(AppLocalizations.of(context)!.confirmClose),
+            content: Text(AppLocalizations.of(context)!.areYouSureExit),
+            actions: [
+              FilledButton(
+                child: Text(AppLocalizations.of(context)!.yesOrAffirmitive),
+                onPressed: () {
+                  Navigator.pop(context);
+                  windowManager.destroy();
+                },
+              ),
+              Button(
+                child: Text(AppLocalizations.of(context)!.noOrCancellation),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}

commit ce3f5a0be64b62482ae1df389be77f7209e8197b

refactor: :art: Chat room refactor + misc fixes
New chat room refactor, timeline and events moved to their own widgets; various styling and code credit fixes throughout; fixed the background colour bug introduced with the new desktop frame

Colour bug introduced in 441604859be516e91cdd045e260c0f8367e6259f

lib/src/chat/basic_text_event.dart
@@ -1,0 +1,32 @@
+// Copyright (C) 2025 Surena Karimpour Ghannadi
+//
+// This file is part of Prject Azhi.
+//
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:flutter/material.dart';
+
+class BasicTextEvent extends StatefulWidget {
+  const BasicTextEvent({super.key});
+
+  @override
+  State<BasicTextEvent> createState() => _BasicTextEventState();
+}
+
+class _BasicTextEventState extends State<BasicTextEvent> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

lib/src/chat/chat_event.dart
@@ -1,0 +1,52 @@
+// Copyright (C) 2025 Surena Karimpour Ghannadi
+//
+// This file is part of Prject Azhi.
+//
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:matrix/matrix.dart';
+
+class AzhiChatEvent extends StatefulWidget {
+  const AzhiChatEvent({super.key, required this.event});
+  final Event event;
+  @override
+  State<AzhiChatEvent> createState() => _AzhiChatEventState();
+}
+
+class _AzhiChatEventState extends State<AzhiChatEvent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: FluentTheme.of(context).accentColor,
+          width: 1,
+          strokeAlign: BorderSide.strokeAlignOutside,
+        ),
+        borderRadius: BorderRadius.circular(
+          12,
+        ),
+        color: FluentTheme.of(context).acrylicBackgroundColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          widget.event.body,
+          style: const TextStyle(fontFamily: 'JetBrainsMono', fontSize: 14),
+        ),
+      ),
+    );
+  }
+}

lib/src/chat/chat_timeline.dart
@@ -1,0 +1,161 @@
+// Copyright (C) 2025 Surena Karimpour Ghannadi
+//
+// This file is part of Prject Azhi.
+//
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:azhi_main/src/chat/chat_event.dart';
+import 'package:azhi_main/src/helpers/date_time_extension.dart';
+import 'package:azhi_main/src/widgets/dynamic_avatar.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:go_router/go_router.dart';
+import 'package:matrix/matrix.dart';
+
+class AzhiChatTimeline extends StatefulWidget {
+  const AzhiChatTimeline({super.key, required this.room});
+  final Room room;
+
+  @override
+  State<AzhiChatTimeline> createState() => _AzhiChatTimelineState();
+}
+
+//TODO: This needs settingsController styling.
+class _AzhiChatTimelineState extends State<AzhiChatTimeline> {
+  late final Future<Timeline> _timelineFuture;
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  final ScrollController _scrollController = ScrollController();
+  // Counts events
+  // ignore: unused_field
+  int _count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Timeline>(
+      future: _timelineFuture,
+      builder: (context, snapshot) {
+        final timeline = snapshot.data;
+        if (timeline == null) {
+          return Center(
+            child: SpinKitCubeGrid(
+              color: FluentTheme.of(context).accentColor,
+            ),
+          );
+        }
+        // Add a listener to the scroll controller
+        _scrollController.addListener(
+          () {
+            if (_scrollController.position.pixels ==
+                _scrollController.position.maxScrollExtent) {
+              // User has scrolled to the bottom, request more data
+              timeline.requestHistory();
+            }
+          },
+        );
+        _count = timeline.events.length;
+        return Expanded(
+          child: AnimatedList(
+            controller: _scrollController,
+            key: _listKey,
+            reverse: true,
+            initialItemCount: timeline.events.length,
+            itemBuilder: (context, index, animation) {
+              return (timeline.events[index].relationshipEventId != null)
+                  ? Container()
+                  : ScaleTransition(
+                      scale: animation,
+                      child: Opacity(
+                        opacity: timeline.events[index].status.isSent ? 1 : 0.5,
+                        child: ListTile(
+                          leading: DynamicAvatarWidget(
+                            client: widget.room.client,
+                            avatarUri: timeline.events[index]
+                                .senderFromMemoryOrFallback.avatarUrl,
+                            onTap: () => context.push(
+                              '${GoRouterState.of(context).uri}/profile/${timeline.events[index].senderFromMemoryOrFallback.id}',
+                            ),
+                          ),
+                          title: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  timeline
+                                      .events[index].senderFromMemoryOrFallback
+                                      .calcDisplayname(),
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Text(
+                                timeline.events[index].originServerTs
+                                    .localizedTimeShort(context),
+                                style: const TextStyle(
+                                  fontSize: 8,
+                                  fontFamily: 'JetBrainsMono',
+                                ),
+                              ),
+                            ],
+                          ),
+                          subtitle: AzhiChatEvent(
+                            event: timeline.events[index],
+                          ),
+                        ),
+                      ),
+                    );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _timelineFuture = widget.room.getTimeline(
+      onChange: (i) {
+        if (kDebugMode) {
+          print('on change! $i');
+        }
+        _listKey.currentState?.setState(() {});
+      },
+      onInsert: (i) {
+        if (kDebugMode) {
+          print('on insert! $i');
+        }
+        _listKey.currentState?.insertItem(i);
+        _count++;
+      },
+      onRemove: (i) {
+        if (kDebugMode) {
+          print('On remove $i');
+        }
+        _count--;
+        _listKey.currentState?.removeItem(i, (_, __) => const ListTile());
+      },
+      onUpdate: () {
+        if (kDebugMode) {
+          print('On update');
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); // Dispose the controller
+    super.dispose();
+  }
+}

lib/src/chat/item.dart
@@ -1,17 +1,0 @@
-// Copyright (C) 2024 Surena Karimpour Ghannadi
-// 
-// This file is part of Prject Azhi.
-// 
-// Prject Azhi is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Prject Azhi is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with Prject Azhi.  If not, see <http://www.gnu.org/licenses/>.
-

lib/src/chat/room_info_card.dart
@@ -31,8 +31,7 @@ class RoomInfoCard extends StatelessWidget {
         context: context,
         barrierDismissible: true,
         builder: (context) => BlurryContainer(
-          blur: 16,
-          elevation: 5,
+          blur: 24,
           child: RoomInformations(room: room),
         ),
       ),

lib/src/chat/users_avatar.dart
@@ -1,17 +1,0 @@
-// Copyright (C) 2024 Surena Karimpour Ghannadi
-// 
-// This file is part of Prject Azhi.
-// 
-// Prject Azhi is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Prject Azhi is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with Prject Azhi.  If not, see <http://www.gnu.org/licenses/>.
-

lib/src/helpers/date_time_extension.dart
@@ -1,3 +1,5 @@
+//Credits: File originally taken from FluffyChat (AGPL v3 license)
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:intl/intl.dart';
 

lib/src/layouts/desktop_app_frame.dart
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
 
-import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:window_manager/window_manager.dart';
@@ -56,18 +55,13 @@ class _AzhiAppFrameState extends State<AzhiAppFrame> with WindowListener {
     return Stack(
       children: [
         DragToResizeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              color: FluentTheme.of(context).accentColor,
-            ),
-          ),
+          child: Container(),
         ),
         ScaffoldPage.withPadding(
-          padding: const EdgeInsets.all(1.5),
+          padding: const EdgeInsets.all(1),
           header: DragToMoveArea(
-            child: BlurryContainer(
-              color: FluentTheme.of(context).accentColor,
-              borderRadius: const BorderRadius.all(Radius.zero),
+            child: Container(
+              color: FluentTheme.of(context).accentColor.darker,
               padding: const EdgeInsets.only(left: 8.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,

lib/src/locations.dart
@@ -85,7 +85,10 @@ class AppLocationsHandler {
             GoRoute(
               path: 'register',
               pageBuilder: (context, state) => azhiPageBuilder(
-                  context, state, const RegisterNewUserAccountGuidancePage()),
+                context,
+                state,
+                const RegisterNewUserAccountGuidancePage(),
+              ),
             ),
           ],
         ),

lib/src/screens/room_page.dart
@@ -16,12 +16,10 @@
 // along with Prject Azhi.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'package:azhi_main/src/chat/chat_box.dart';
+import 'package:azhi_main/src/chat/chat_timeline.dart';
 import 'package:azhi_main/src/chat/room_info_card.dart';
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' as mt;
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
 
 class FluentRoomPage extends StatefulWidget {
@@ -32,13 +30,6 @@ class FluentRoomPage extends StatefulWidget {
 }
 
 class _FluentRoomPageState extends State<FluentRoomPage> {
-  late final Future<Timeline> _timelineFuture;
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  final ScrollController _scrollController = ScrollController();
-  // Counts events
-  // ignore: unused_field
-  int _count = 0;
-
   @override
   Widget build(BuildContext context) {
     return mt.Scaffold(
@@ -53,135 +44,7 @@ class _FluentRoomPageState extends State<FluentRoomPage> {
             direction: Axis.horizontal,
           ),
           Expanded(
-            child: FutureBuilder<Timeline>(
-              future: _timelineFuture,
-              builder: (context, snapshot) {
-                final timeline = snapshot.data;
-                if (timeline == null) {
-                  return Center(
-                    child: SpinKitCubeGrid(
-                      color: FluentTheme.of(context).accentColor,
-                    ),
-                  );
-                }
-                // Add a listener to the scroll controller
-                _scrollController.addListener(
-                  () {
-                    if (_scrollController.position.pixels ==
-                        _scrollController.position.maxScrollExtent) {
-                      // User has scrolled to the bottom, request more data
-                      timeline.requestHistory();
-                    }
-                  },
-                );
-                _count = timeline.events.length;
-                return Column(
-                  children: [
-                    const Divider(
-                      direction: Axis.horizontal,
-                      size: 2,
-                    ),
-                    Expanded(
-                      child: AnimatedList(
-                        controller: _scrollController,
-                        key: _listKey,
-                        reverse: true,
-                        initialItemCount: timeline.events.length,
-                        itemBuilder: (context, index, animation) {
-                          return (timeline.events[index].relationshipEventId !=
-                                  null)
-                              ? Container()
-                              : ScaleTransition(
-                                  scale: animation,
-                                  child: Opacity(
-                                    opacity:
-                                        timeline.events[index].status.isSent
-                                            ? 1
-                                            : 0.5,
-                                    child: ListTile(
-                                      leading: GestureDetector(
-                                        onTap: () => context.push(
-                                            '${GoRouterState.of(context).uri}/profile/${timeline.events[index].senderFromMemoryOrFallback.id}'),
-                                        child: CircleAvatar(
-                                          foregroundImage: timeline
-                                                      .events[index]
-                                                      .senderFromMemoryOrFallback
-                                                      .avatarUrl ==
-                                                  null
-                                              ? null
-                                              : NetworkImage(
-                                                  timeline
-                                                      .events[index]
-                                                      .senderFromMemoryOrFallback
-                                                      .avatarUrl!
-                                                      .getThumbnail(
-                                                        widget.room.client,
-                                                        width: 56,
-                                                        height: 56,
-                                                      )
-                                                      .toString(),
-                                                ),
-                                        ),
-                                      ),
-                                      title: Row(
-                                        children: [
-                                          Expanded(
-                                            child: Text(
-                                              timeline.events[index]
-                                                  .senderFromMemoryOrFallback
-                                                  .calcDisplayname(),
-                                              style: const TextStyle(
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                          ),
-                                          Text(
-                                            timeline
-                                                .events[index].originServerTs
-                                                .toIso8601String(),
-                                            style: const TextStyle(
-                                                fontSize: 8,
-                                                fontFamily: 'JetBrainsMono'),
-                                          ),
-                                        ],
-                                      ),
-                                      subtitle: Container(
-                                        decoration: BoxDecoration(
-                                          border: Border.all(
-                                            color: FluentTheme.of(context)
-                                                .accentColor,
-                                            width: 1,
-                                            strokeAlign:
-                                                BorderSide.strokeAlignOutside,
-                                          ),
-                                          borderRadius: BorderRadius.circular(
-                                            12,
-                                          ),
-                                          color: FluentTheme.of(context)
-                                              .acrylicBackgroundColor,
-                                        ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text(
-                                            timeline.events[index]
-                                                .getDisplayEvent(timeline)
-                                                .body,
-                                            style: const TextStyle(
-                                                fontFamily: 'JetBrainsMono',
-                                                fontSize: 14),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                        },
-                      ),
-                    )
-                  ],
-                );
-              },
-            ),
+            child: AzhiChatTimeline(room: widget.room),
           ),
           const Divider(
             direction: Axis.vertical,
@@ -191,43 +54,5 @@ class _FluentRoomPageState extends State<FluentRoomPage> {
         ],
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _timelineFuture = widget.room.getTimeline(
-      onChange: (i) {
-        if (kDebugMode) {
-          print('on change! $i');
-        }
-        _listKey.currentState?.setState(() {});
-      },
-      onInsert: (i) {
-        if (kDebugMode) {
-          print('on insert! $i');
-        }
-        _listKey.currentState?.insertItem(i);
-        _count++;
-      },
-      onRemove: (i) {
-        if (kDebugMode) {
-          print('On remove $i');
-        }
-        _count--;
-        _listKey.currentState?.removeItem(i, (_, __) => const ListTile());
-      },
-      onUpdate: () {
-        if (kDebugMode) {
-          print('On update');
-        }
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose(); // Dispose the controller
-    super.dispose();
   }
 }

lib/src/widgets/dynamic_avatar.dart
@@ -1,0 +1,65 @@
+// Copyright (C) 2024 Surena Karimpour Ghannadi
+//
+// This file is part of Prject Azhi.
+//
+// Prject Azhi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Prject Azhi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Prject Azhi.  If not, see <http://www.gnu.org/licenses/>.
+
+// TODO: Loading animations, handle different states, theming?
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:matrix/matrix.dart';
+
+enum AvatarStates {
+  inactive,
+  active,
+}
+
+class DynamicAvatarWidget extends StatefulWidget {
+  const DynamicAvatarWidget({
+    super.key,
+    required this.client,
+    this.avatarUri,
+    this.onTap,
+    this.fallbackImage,
+  });
+  final Client client;
+  final Uri? avatarUri;
+  final Function? onTap;
+  final AvatarStates avatarState = AvatarStates.active;
+  final ImageProvider? fallbackImage;
+  @override
+  State<DynamicAvatarWidget> createState() => _DynamicAvatarWidgetState();
+}
+
+class _DynamicAvatarWidgetState extends State<DynamicAvatarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => widget.onTap,
+      child: CircleAvatar(
+        foregroundImage: widget.avatarUri == null
+            // TODO: Default Icon!
+            ? widget.fallbackImage
+            : NetworkImage(
+                widget.avatarUri!
+                    .getThumbnail(
+                      widget.client,
+                      width: 56,
+                      height: 56,
+                    )
+                    .toString(),
+              ),
+      ),
+    );
+  }
+}