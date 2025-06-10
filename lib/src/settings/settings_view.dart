2d143a7693db0740d9f93fbd8803da0acd5d8fef
Surena Karimpour Ghannadi <suren.karimp@gmail.com>
2025-06-10 04:05:33 +0330

    feat: :sparkles: New tabbed settings UI + New settings added

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -1,4 +1,3 @@
-import 'package:moonrelay/src/layouts/app_frame.dart';
 import 'package:moonrelay/src/layouts/custom_scaffold.dart';
 import 'package:moonrelay/src/settings/display_type.dart';
 import 'package:fluent_ui/fluent_ui.dart';
@@ -22,13 +21,14 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
-  double? _transparencySliderValue;
   @override
   Widget build(BuildContext context) {
+    int currentTabIndex = 0;
     return Consumer<SettingsController>(
       builder: (context, controller, child) {
-        return CustomScaffold(
-          topBar: Padding(
+        return NavigationView(
+          appBar: NavigationAppBar(
+              leading: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
@@ -45,121 +45,154 @@ class _SettingsViewState extends State<SettingsView> {
                 )
               ],
             ),
-          ),
+          )),
           content: Padding(
             padding: const EdgeInsets.all(16),
             // Glue the SettingsController to the theme selection DropdownButton.
             //
             // When a user selects a theme from the dropdown list, the
             // SettingsController is updated, which rebuilds the MaterialApp.
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                DropDownButton(
-                  leading: const Text("Application Theme"),
-                  // Call the updateThemeMode method any time the user selects a theme.
-
-                  items: [
-                    MenuFlyoutItem(
-                      text: const Text("System Theme"),
-                      onPressed: () =>
-                          controller.updateThemeMode(ThemeMode.system),
-                    ),
-                    MenuFlyoutItem(
-                        text: const Text("Light Theme"),
-                        onPressed: () =>
-                            controller.updateThemeMode(ThemeMode.light)),
-                    MenuFlyoutItem(
-                        text: const Text("Dark Theme"),
-                        onPressed: () =>
-                            controller.updateThemeMode(ThemeMode.dark)),
-                  ],
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                DropDownButton(
-                  leading: const Text("Chat display type"),
-                  items: [
-                    MenuFlyoutItem(
-                      text: Text(DisplayType.modern.label),
-                      onPressed: () =>
-                          controller.updateDisplayType(DisplayType.modern),
-                    ),
-                    MenuFlyoutItem(
-                      text: Text(DisplayType.irc.label),
-                      onPressed: () =>
-                          controller.updateDisplayType(DisplayType.irc),
-                    ),
-                    MenuFlyoutItem(
-                      text: Text(DisplayType.bubbles.label),
-                      onPressed: () =>
-                          controller.updateDisplayType(DisplayType.bubbles),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                Text(
-                  'Linux only supports Transparent and solid. Use your WM to apply blur to transparency.',
-                  style: TextStyle(fontSize: 16, fontFamily: 'Rubik'),
-                ),
-                DropDownButton(
-                  leading: const Text("Window Effect"),
-                  items: [
-                    MenuFlyoutItem(
-                      text: Text(WindowEffect.acrylic.name),
-                      onPressed: () =>
-                          controller.updateWindowEffect(WindowEffect.acrylic),
-                    ),
-                    MenuFlyoutItem(
-                      text: Text(WindowEffect.transparent.name),
-                      onPressed: () => controller
-                          .updateWindowEffect(WindowEffect.transparent),
-                    ),
-                    MenuFlyoutItem(
-                      text: Text(WindowEffect.mica.name),
-                      onPressed: () =>
-                          controller.updateWindowEffect(WindowEffect.mica),
-                    ),
-                    MenuFlyoutItem(
-                      text: Text(WindowEffect.solid.name),
-                      onPressed: () =>
-                          controller.updateWindowEffect(WindowEffect.solid),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                Text(
-                  'Background transparency value. Higher means more solid.',
-                  style: const TextStyle(fontSize: 16, fontFamily: 'Rubik'),
-                ),
-                SizedBox(
-                  height: 6.0,
-                ),
-                Slider(
-                  label: 'Background transparency',
-                  value: _transparencySliderValue ??
-                      controller.backgroundTransparencyScalar.toDouble(),
-                  min: 0,
-                  max: 255,
-                  onChanged: (double value) {
-                    setState(() {
-                      controller
-                          .updateBackgroundTransparencyScalar(value.toInt());
-                      _transparencySliderValue = value;
-                    });
-                  },
-                )
-              ],
-            ),
+            child: TabView(
+                currentIndex: currentTabIndex,
+                onChanged: (index) => setState(() => currentTabIndex = index),
+                tabWidthBehavior: TabWidthBehavior.equal,
+                showScrollButtons: true,
+                tabs: [
+                  Tab(text: Text('old'), body: OldPage(controller: controller))
+                ]),
           ),
         );
       },
+    );
+  }
+}
+
+class OldPage extends StatefulWidget {
+  const OldPage({super.key, required this.controller});
+  final SettingsController controller;
+
+  @override
+  State<OldPage> createState() => _OldPageState();
+}
+
+class _OldPageState extends State<OldPage> {
+  double? _transparencySliderValue;
+
+  @override
+  Widget build(BuildContext context) {
+    bool systemBar = widget.controller.useSystemTitlebar;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        DropDownButton(
+          leading: const Text("Application Theme"),
+          // Call the updateThemeMode method any time the user selects a theme.
+
+          items: [
+            MenuFlyoutItem(
+              text: const Text("System Theme"),
+              onPressed: () =>
+                  widget.controller.updateThemeMode(ThemeMode.system),
+            ),
+            MenuFlyoutItem(
+                text: const Text("Light Theme"),
+                onPressed: () =>
+                    widget.controller.updateThemeMode(ThemeMode.light)),
+            MenuFlyoutItem(
+                text: const Text("Dark Theme"),
+                onPressed: () =>
+                    widget.controller.updateThemeMode(ThemeMode.dark)),
+          ],
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Checkbox(
+          checked: systemBar,
+          onChanged: (value) =>
+              widget.controller.updateUseOfSystemTitlebar(value!),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        DropDownButton(
+          leading: const Text("Chat display type"),
+          items: [
+            MenuFlyoutItem(
+              text: Text(DisplayType.modern.label),
+              onPressed: () =>
+                  widget.controller.updateDisplayType(DisplayType.modern),
+            ),
+            MenuFlyoutItem(
+              text: Text(DisplayType.irc.label),
+              onPressed: () =>
+                  widget.controller.updateDisplayType(DisplayType.irc),
+            ),
+            MenuFlyoutItem(
+              text: Text(DisplayType.bubbles.label),
+              onPressed: () =>
+                  widget.controller.updateDisplayType(DisplayType.bubbles),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Text(
+          'Linux only supports Transparent and solid. Use your WM to apply blur to transparency.',
+          style: TextStyle(fontSize: 16, fontFamily: 'Rubik'),
+        ),
+        DropDownButton(
+          leading: const Text("Window Effect"),
+          items: [
+            MenuFlyoutItem(
+              text: Text(WindowEffect.acrylic.name),
+              onPressed: () =>
+                  widget.controller.updateWindowEffect(WindowEffect.acrylic),
+            ),
+            MenuFlyoutItem(
+              text: Text(WindowEffect.transparent.name),
+              onPressed: () => widget.controller
+                  .updateWindowEffect(WindowEffect.transparent),
+            ),
+            MenuFlyoutItem(
+              text: Text(WindowEffect.mica.name),
+              onPressed: () =>
+                  widget.controller.updateWindowEffect(WindowEffect.mica),
+            ),
+            MenuFlyoutItem(
+              text: Text(WindowEffect.solid.name),
+              onPressed: () =>
+                  widget.controller.updateWindowEffect(WindowEffect.solid),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        Text(
+          'Background transparency value. Higher means more solid.',
+          style: const TextStyle(fontSize: 16, fontFamily: 'Rubik'),
+        ),
+        SizedBox(
+          height: 6.0,
+        ),
+        Slider(
+          label: 'Background transparency',
+          value: _transparencySliderValue ??
+              widget.controller.backgroundTransparencyScalar.toDouble(),
+          min: 0,
+          max: 255,
+          onChanged: (double value) {
+            setState(() {
+              widget.controller
+                  .updateBackgroundTransparencyScalar(value.toInt());
+              _transparencySliderValue = value;
+            });
+          },
+        )
+      ],
     );
   }
 }
