93c13f0ee04b60f0d664eb9ce6feba84bd0c5719
Surena Karimpour Ghannadi <suren.karimp@gmail.com>
2025-06-10 04:09:01 +0330

    feat: :sparkles: New custom title bar

--- lib/src/layouts/app_frame.dart
+++ lib/src/layouts/app_frame.dart
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:moonrelay/src/helpers/color_palette.dart';
 import 'package:moonrelay/src/layouts/custom_scaffold.dart';
 import 'package:moonrelay/src/localization/app_localizations.dart';
@@ -22,7 +23,6 @@ import 'package:moonrelay/src/widgets/blur_background.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:provider/provider.dart';
 import 'package:window_manager/window_manager.dart';
-import 'package:moonrelay/src/widgets/window_buttons.dart';
 
 class AppFrame extends StatefulWidget {
   const AppFrame({
@@ -62,15 +62,21 @@ class _AppFrameState extends State<AppFrame> with WindowListener {
         //   child: Container(),
         // ),
 
-        // FIXME - Style this from settings controller
         Consumer<SettingsController>(
           builder: (context, value, child) => CustomScaffold(
-            backgroundColor: (MediaQuery.platformBrightnessOf(context).isDark)
+            backgroundColor: (value.themeMode == ThemeMode.dark)
                 ? MoonrelayColorPalette.cpgDarkest
                     .withAlpha(value.backgroundTransparencyScalar)
                 : MoonrelayColorPalette.cpgWhite
                     .withAlpha(value.backgroundTransparencyScalar),
-            topBar: BlurBackground(child: TitleBar()),
+            topBar: value.useSystemTitlebar
+                ? null
+                : BlurBackground(
+                    child: TitleBar(
+                        brightness: (value.themeMode == ThemeMode.dark)
+                            ? Brightness.dark
+                            : Brightness.light),
+                  ),
             content: widget.child,
           ),
         ),
@@ -110,13 +116,56 @@ class _AppFrameState extends State<AppFrame> with WindowListener {
   }
 }
 
-class TitleBar extends StatelessWidget {
+class TitleBar extends StatefulWidget {
   const TitleBar({
     super.key,
+    required this.brightness,
   });
+
+  final Brightness brightness;
+
+  @override
+  State<TitleBar> createState() => _TitleBarState();
+}
+
+class _TitleBarState extends State<TitleBar> with WindowListener {
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  void onWindowMaximize() {
+    setState(() {});
+  }
+
+  @override
+  void onWindowUnmaximize() {
+    setState(() {});
+  }
+
+  @override
+  void onWindowMinimize() {
+    setState(() {});
+    super.onWindowMinimize();
+  }
+
+  @override
+  void onWindowRestore() {
+    setState(() {});
+    super.onWindowRestore();
+  }
 
   @override
   Widget build(BuildContext context) {
+    bool isDark = (widget.brightness == Brightness.dark);
     return DragToMoveArea(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -129,10 +178,80 @@ class TitleBar extends StatelessWidget {
                 fontFamily: 'Oxanium',
                 fontWeight: FontWeight.bold,
                 fontSize: 16,
+                color: MoonrelayColorPalette.the90sBrick,
               ),
             ),
           ),
-          const WindowButtons(),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 6.0),
+            child: Container(
+              width: 156,
+              height: 32,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: isDark
+                          ? MoonrelayColorPalette.the90sBrick
+                          : MoonrelayColorPalette.cpgDark),
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  FutureBuilder<bool>(
+                    future: windowManager.isMinimized(),
+                    builder: (context, AsyncSnapshot<bool> snapshot) {
+                      if (snapshot.data == true) {
+                        return IconButton(
+                          icon: Icon(LucideIcons.maximize2),
+                          onPressed: () => windowManager.restore(),
+                        );
+                      } else {
+                        return IconButton(
+                          icon: Icon(LucideIcons.minimize2),
+                          onPressed: () => windowManager.minimize(),
+                        );
+                      }
+                    },
+                  ),
+                  FutureBuilder<bool>(
+                    future: windowManager.isMaximized(),
+                    builder:
+                        (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                      if (snapshot.data == true) {
+                        return IconButton(
+                          icon: Icon(LucideIcons.minimize),
+                          onPressed: () => windowManager.unmaximize(),
+                        );
+                      }
+                      return IconButton(
+                        icon: Icon(LucideIcons.maximize),
+                        onPressed: () => windowManager.maximize(),
+                      );
+                    },
+                  ),
+                  IconButton(
+                    style: ButtonStyle(backgroundColor:
+                        WidgetStateProperty.resolveWith<Color?>(
+                      (states) {
+                        if (states.contains(WidgetState.hovered) ||
+                            states.contains(WidgetState.focused)) {
+                          return MoonrelayColorPalette.brightMaroon
+                              .withAlpha(128);
+                        }
+                        if (states.contains(WidgetState.pressed)) {
+                          return MoonrelayColorPalette.brightMaroon
+                              .withAlpha(255);
+                        }
+                        return null;
+                      },
+                    )),
+                    icon: Icon(LucideIcons.squareX),
+                    onPressed: () => windowManager.close(),
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
